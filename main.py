6d1911f5a6e9e673b0efe1169960aea546980cd5
sgomeza13 <67118511+sgomeza13@users.noreply.github.com>
2023-05-24 22:26:54 -0500

    Add files via upload
    Implementacion de pool mala

--- main.py
+++ main.py
@@ -1,21 +1,32 @@
 from pydub import AudioSegment
+from multiprocessing import Pool
 
-def combinar_canciones(cancion1, cancion2, salida):
+def combinar_canciones(cancion1):
     # Cargar las canciones utilizando pydub
     cancion1 = AudioSegment.from_mp3("/home/simon/dev/organizacion_computadores/proyecto_audio/files/Duvet.mp3")
     cancion2 = AudioSegment.from_mp3("/home/simon/dev/organizacion_computadores/proyecto_audio/files/UN_OWEN_WAS_HER.mp3")
 
+    
     # Combinar las canciones
-    cancion_combinada = cancion1 + cancion2
+    cancion_combinada = cancion1.overlay(cancion2)
 
     # Guardar la canción combinada en un archivo MP3
-    cancion_combinada.export(salida, format='mp3')
+    cancion_combinada.export("/home/simon/dev/organizacion_computadores/proyecto_audio/files/f3.mp3", format='mp3')
 
     print("¡Las canciones se han combinado correctamente!")
+    return cancion_combinada
 
 # Ejemplo de uso
-cancion1 = "cancion1.mp3"
-cancion2 = "cancion2.mp3"
-salida = "canciones_combinadas.mp3"
 
-combinar_canciones(cancion1, cancion2, salida)
+
+
+def main():
+    cancion1 = "cancion1.mp3"
+    cancion2 = "cancion2.mp3"
+    with Pool() as pool:
+        results = pool.map(combinar_canciones,cancion1)
+    #combinar_canciones(cancion1)
+
+
+if __name__ == '__main__':
+    main()
--- test.py
+++ test.py
@@ -16,8 +16,9 @@ def audio_mixer(f1,f2):
     print (struct.unpack("{}b".format(len(entry2)), entry2))
 
 
-    third_mp3 = (first_mp3+second_mp3)
-    with open("/home/simon/dev/organizacion_computadores/proyecto_audio/files/f3.mp3","wb") as f_mp3:
-        f_mp3.write(third_mp3)
 
-audio_mixer("/home/simon/dev/organizacion_computadores/proyecto_audio/files/Duvet.mp3","/home/simon/dev/organizacion_computadores/proyecto_audio/files/UN_OWEN_WAS_HER.mp3")
+    with open("/home/simon/dev/organizacion_computadores/proyecto_audio2/files/f3.mp3","wb") as f_mp3:
+        for i in first_mp3:
+            f_mp3.write(bytes(first_mp3[i]+second_mp3[i]))
+
+audio_mixer("/home/simon/dev/organizacion_computadores/proyecto_audio2/files/Duvet.mp3","/home/simon/dev/organizacion_computadores/proyecto_audio2/files/UN_OWEN_WAS_HER.mp3")
